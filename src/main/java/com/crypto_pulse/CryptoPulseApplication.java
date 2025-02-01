a2001e72b4e0215a3a2c271dce090c58dc7da1c1
Serhii Manko <sergii_manko@gmail.com>
2025-02-01 22:56:43 +0100

    Initial commit

---
+++ src/main/java/com/crypto_pulse/CryptoPulseApplication.java
@@ -0,0 +1,13 @@
+package com.crypto_pulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoPulseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crypto_pulse/CryptoPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto_pulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
